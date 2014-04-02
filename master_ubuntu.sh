9ebb24a7323a38174305933b0ef72bc778764141
Ah-Lun Tang <tang@ahlun.be>
2014-04-02 21:23:32 +0200

    ubuntu install script

---
+++ master_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+sudo dpkg -i puppetlabs-release-precise.deb
+sudo apt-get update
+sudo apt-get install puppetmaster puppetmaster-passenger
+sudo puppet resource service puppetmaster ensure=running enable=true
